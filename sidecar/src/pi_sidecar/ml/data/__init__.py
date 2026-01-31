42fdc2f0aba1d307ed002a228f87d8a8be1ce37c
ACFPeacekeeper <afonso.fernandes100@gmail.com>
2026-01-31 07:35:21 +0000

    Added ml as a submodule

--- sidecar/src/pi_sidecar/ml/data/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Data loading and dataset definitions.
-"""
-
-from pi_sidecar.ml.data.polymarket_dataset import PolymarketDataset
-from pi_sidecar.ml.data.time_series_dataset import TimeSeriesDataset
-
-__all__ = ["PolymarketDataset", "TimeSeriesDataset"]
